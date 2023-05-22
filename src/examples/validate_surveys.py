29219fd27a2eba49fe42529bc0cbfef5c1b55662
pockerman <a.giavaras@gmail.com>
2023-05-22 14:21:17 +0100

    Update API

--- src/examples/validate_surveys.py
+++ src/examples/validate_surveys.py
@@ -2,7 +2,11 @@ from pathlib import Path
 from loguru import logger
 from src.mir_survey_utils.survey_validators.condition_survey_validator import ConditionSurveyValidator
 from src.mir_survey_utils.utils.build_checkpoint_to_survey_item_map import build_checkpoint_to_survey_item_map
-
+from src.mir_survey_utils.survey_transformers.findings_transformers import (trim_finding_properties,
+                                                                            set_checkpoint_group_to_mir_type,
+                                                                            replace_findings_severity,
+                                                                            set_checkpoint_item_to_checkpoint_group,
+                                                                            replace_checkpoint_group_name)
 
 if __name__ == '__main__':
 
@@ -15,9 +19,38 @@ if __name__ == '__main__':
 
     logger.info(f"Survey {survey_path} is valid={survey.is_valid}")
 
+
     checkpoint_to_survey_item_map = build_checkpoint_to_survey_item_map(path_to_checkpoints=Path("/home/alex/qi3/nv_nlp_utils/checkpoint_json_files"))
+
+    print(checkpoint_to_survey_item_map)
 
     # fix survey according to mir format
     logger.info(f"Loaded {len(checkpoint_to_survey_item_map)} survey items")
+
+    updated = replace_findings_severity(survey=survey, old_severity_value="ACCEPTABLE", new_severity_value="MINOR")
+    logger.info(f"Updated {updated} severity levels")
+
+    replace_checkpoint_group_name(survey, checkpoint_group_name={'ANCHORS': 'ANCHOR',
+                                                                 })
+
+    # apply transformations to the valid survey
+    trim_finding_properties(survey, finding_properties=['checkpoint_item', 'checkpoint_group'])
+    not_found = set_checkpoint_group_to_mir_type(survey, checkpoint_group_map=checkpoint_to_survey_item_map)
+
+    if len(not_found) != 0:
+        logger.warning(f"Couldn't find mir survey item for {len(not_found)} checkpoints")
+        logger.warning(not_found)
+
+        set_checkpoint_item_to_checkpoint_group(survey=survey,
+                                                checkpoint_item_to_checkpoint_group={'OIL_COOLERS': 'PROPULSION_MACHINERY',
+                                                                                     'EXHAUST_SYSTEM': 'PROPULSION_MACHINERY',
+                                                                                     'ANCHORS': 'DECK_EQUIPMENT',
+                                                                                     'HALYARDS':'RIG_AND_SAILS'})
+    else:
+        logger.info("All checkpoints have been identified")
+
+    # save the valid survey
+    filename = survey.filename
+    survey.save(filename=Path(f"/home/alex/qi3/nv_nlp_utils/tmp_files/{filename}"))
 
 
--- src/mir_survey_utils/survey_validators/condition_survey_validator.py
+++ src/mir_survey_utils/survey_validators/condition_survey_validator.py
@@ -1,14 +1,7 @@
 from pathlib import Path
+from typing import Union
 import json
-from loguru import logger
-from typing import Union, List
-from pydantic import Field
-from src.mir_survey_utils.survey_schemas.engine_data_schema import Engine, EngineEntry
-from src.mir_survey_utils.survey_schemas.vessel_data_schema import VesselDim, VesselDataSchema
-from src.mir_survey_utils.survey_schemas.survey_data_schema import SurveyDataSchema, SeverityScale
-from src.mir_survey_utils.survey_schemas.finding_data_schema import FindingDataSchema
-from src.mir_survey_utils.survey_schemas.survey_item_data_schema import SurveyItemDataSchema
-from src.mir_survey_utils.survey_schemas.checkpoint_data_schema import CheckpointDataSchema
+from .condition_survey_validator_pimpl import ConditionSurveyValidatorPimpl
 
 OK = "OK"
 NOT_OK = "NOT_OK"
@@ -17,211 +10,43 @@ NOT_OK = "NOT_OK"
 class ConditionSurveyValidator(object):
 
     def __init__(self, survey_json_doc: Path = None, validate: bool = False):
-        self.survey = None
-        self.source: Path = survey_json_doc
-        self.is_valid: bool = False
-        self.valid_survey = {}
 
-        if survey_json_doc is not None:
-            self.load(source=self.source)
+        self.pimpl = ConditionSurveyValidatorPimpl(survey_json_doc=survey_json_doc,
+                                                   validate=validate)
 
-            if validate:
-                self.validate()
+    @property
+    def is_valid(self) -> bool:
+        return self.pimpl.is_valid
 
-    def load(self, source: Path):
+    @property
+    def survey(self) -> dict:
+        return self.pimpl.survey
 
-        with open(source, 'r') as f:
-            self.survey = json.load(f)
-            self.source = source
+    @property
+    def source(self) -> Path:
+        return self.pimpl.source
+
+    @property
+    def filename(self) -> str:
+        return self.source.name
+
+    @property
+    def valid_survey(self) -> dict:
+
+        if not self.is_valid:
+            raise ValueError("The survey data is not valid")
+
+        return self.pimpl.valid_survey
+
+    def load(self, source: Path) -> None:
+        self.pimpl._load(source=source)
 
     def validate(self) -> Union[OK, NOT_OK]:
-        try:
+        return self.pimpl._validate()
 
-            validate_survey_data = self._validate_survey_data()
+    def save(self, filename: Path) -> None:
+        if filename.suffix != '.json':
+            raise ValueError("json format is expected here.")
 
-            if validate_survey_data != OK:
-                raise ValueError("Survey data was not validated")
-
-            validate_vessel_data = self._validate_vessel_data()
-
-            if validate_vessel_data != OK:
-                raise ValueError("Vessel data was not validated")
-
-            validate_engine = self._validate_engine()
-
-            if validate_engine != OK:
-                raise ValueError("Vessel engine was not validated")
-
-            validate_survey_items_data = self._validate_survey_items_data()
-
-            if validate_survey_items_data != OK:
-                raise ValueError("Survey validate_survey_items_data not validated")
-
-            validate_checkpoint_data = self._validate_survey_checkpoints_data()
-
-            if validate_checkpoint_data != OK:
-                raise ValueError("Survey checkpoint_synopsis_data not validated")
-
-            validate_findings = self._validate_findings()
-
-            if validate_findings != OK:
-                raise ValueError("Survey findings not validated")
-
-            self.is_valid = True
-        except Exception as e:
-
-            logger.error(f"An error occurred whilst validating survey document {str(e)}")
-            self.is_valid = False
-            return NOT_OK
-
-    def _validate_survey_data(self) -> Union[OK, NOT_OK]:
-        try:
-            logger.info("Validating survey data...")
-            survey_data = self.survey["survey_data"]
-
-            severity_scales = survey_data["severity_scales"]
-            severity_scales_arr: List[SeverityScale] = []
-
-            for item in severity_scales:
-                severity_scales_arr.append(SeverityScale(**{"name": item["type"],
-                                                            "explanation": item["explanation"]}))
-
-            survey_data = SurveyDataSchema(**{"severity_scales": severity_scales_arr,
-                                              "survey_doc_id": survey_data["survey_doc_id"],
-                                              "survey_type": survey_data["survey_type"],
-                                              "survey_summary": survey_data["survey_summary"],
-                                              "survey_purpose": survey_data["survey_purpose"]
-                                              })
-
-            self.valid_survey["survey_data"] = survey_data
-            logger.info("Validating survey data. DONE")
-            return OK
-        except Exception as e:
-            logger.error(f"An error occurred whilst validating vessel data {str(e)}")
-            return NOT_OK
-
-    def _validate_vessel_data(self) -> Union[OK, NOT_OK]:
-        try:
-            logger.info("Validating vessel_data...")
-            vessel_data = self.survey["vessel_data"]
-
-            survey_dimensions = vessel_data["dimensions"]
-            dimensions: List[VesselDim] = []
-
-            for item in survey_dimensions:
-
-                keys = list(item.keys())
-                keys.remove("units")
-
-                if len(keys) != 1:
-                    raise ValueError(f"Unrecognised format for item in survey dimensions. Item was={item}")
-
-                dimensions.append(VesselDim(**{"metric": item["units"],
-                                               "name": keys[0],
-                                               "dimension": item[keys[0]]}))
-
-            vessel_data = VesselDataSchema(**{"vessel_dimensions": dimensions,
-                                              "vessel_name": vessel_data["vessel_name"],
-                                              "hull_material": vessel_data["hull_material"],
-                                              "hin": vessel_data["hin"],
-                                              "sea_category": vessel_data["sea_category"],
-                                              "vessel_type": vessel_data["vessel_type"],
-                                              "model_year": vessel_data["model_year"],
-                                              "survey_vessel_type": vessel_data["survey_vessel_type"],
-                                              "survey_vessel_model": vessel_data["survey_vessel_model"],
-                                              "ssr": vessel_data["ssr"],
-                                              "max_n_persons": vessel_data["max_n_persons"],
-                                              "vessel_usage_type": vessel_data["vessel_usage_type"]})
-
-            self.valid_survey["vessel_data"] = vessel_data
-            logger.info("Validating vessel_data.DONE")
-            return OK
-        except Exception as e:
-            logger.error(f"An error occurred whilst validating vessel data {str(e)}")
-            return NOT_OK
-
-    def _validate_engine(self) -> Union[OK, NOT_OK]:
-
-        try:
-            logger.info("Validating engine...")
-            vessel_data = self.survey["vessel_data"]
-            engine = vessel_data["engine"]
-
-            # how many engines we have
-            n_engines = engine['number_of_engines']
-
-            if n_engines <= 0:
-                raise ValueError(f"Document has zero number of engines")
-
-            engines: List[EngineEntry] = []
-
-            for engine in engine['engines']:
-                engines.append(EngineEntry(**engine))
-
-            if len(engines) != n_engines:
-                raise ValueError(f"The specified number of engines {n_engines} does "
-                                 f"not equal the entries found {len(engines)}")
-
-            vessel_engine = Engine(**{"number_of_engines": n_engines,
-                                      "engines": engines})
-
-            self.valid_survey["vessel_engine"] = vessel_engine
-            logger.info("Validating engine. DONE")
-            return OK
-        except Exception as e:
-            logger.error(f"An error occurred whilst validating engine {str(e)}")
-            return NOT_OK
-
-    def _validate_findings(self):
-
-        try:
-            logger.info("Validating findings...")
-            findings = self.survey['findings_data']
-
-            for finding in findings:
-                finding = FindingDataSchema(**finding)
-
-            self.valid_survey['findings_data'] = findings
-            logger.info("Validating findings. DONE")
-            return OK
-        except Exception as e:
-            logger.error(f"An error occurred whilst validating findings. {str(e)}")
-            return NOT_OK
-
-    def _validate_survey_checkpoints_data(self):
-
-        try:
-            logger.info("Validating survey checkpoint_synopsis_data...")
-            checkpoint_synopsis_data = self.survey['checkpoint_synopsis_data']
-
-            if checkpoint_synopsis_data is None:
-                checkpoint_synopsis_data = []
-
-            for survey_item in checkpoint_synopsis_data:
-                survey_item = CheckpointDataSchema(**survey_item)
-
-            self.valid_survey['checkpoint_synopsis_data'] = checkpoint_synopsis_data
-            logger.info("Validating survey checkpoint_synopsis_data. DONE")
-            return OK
-        except Exception as e:
-            logger.error(f"An error occurred whilst validating checkpoint_synopsis_data. {str(e)}")
-            return NOT_OK
-
-    def _validate_survey_items_data(self):
-
-        try:
-            logger.info("Validating survey items data...")
-            survey_items_data = self.survey['survey_items_data']
-
-            if survey_items_data is None:
-                survey_items_data = []
-
-            for survey_item in survey_items_data:
-                survey_item = SurveyItemDataSchema(**survey_item)
-
-            self.valid_survey['survey_items_data'] = survey_items_data
-            logger.info("Validating survey items data. DONE")
-            return OK
-        except Exception as e:
-            logger.error(f"An error occurred whilst validating survey_items_data. {str(e)}")
-            return NOT_OK
+        with open(filename, 'w') as f:
+            json.dump(self.valid_survey, f)
